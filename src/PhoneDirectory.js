a83ac5e7f303ba998ce55abea05ce03f3966705b
varunsinglaa <varunsingla1999@gmail.com>
2019-12-08 11:35:46 +0530

    Added Delete Subscriber

--- src/PhoneDirectory.js
+++ src/PhoneDirectory.js
@@ -22,6 +22,18 @@ import React, { Component } from 'react';
              ]
          }
      }
+     deleteSubscriberHandler = (subscriberId) => {
+        let subscribersList = this.state.subscribersList;
+        let subscriberIndex = 0;
+        subscribersList.forEach(function (subscriber, index) {
+            if (subscriber.id === subscriberId) {
+                subscriberIndex = index;
+            }
+        }, this);
+        let newSubscribers = subscribersList;
+        newSubscribers.splice(subscriberIndex, 1);
+        this.setState({subscribers: newSubscribers});
+    }
 
      addSubscriberHandler = (newSubscriber) => {
          let subscribersList = this.state.subscribersList;
@@ -39,8 +51,8 @@ import React, { Component } from 'react';
             
              <Router>
                  <div>
-                     <Route exact path="/" render={(props) => <ShowSubscribers {...props} subscribersList={this.state.subscribersList} />} />
-                     <Route exact path="/add" render={({history}, props) => <AddSubscriber history={history} {...props} addSubscriberHandler={this.addSubscriberHandler} />} />
+                 <Route exact path="/" render={(props) => <ShowSubscribers {...props} subscribersList={this.state.subscribersList} deleteSubscriberHandler={this.deleteSubscriberHandler} />} />
+                 <Route exact path="/add" render={({history}, props) => <AddSubscriber history={history} {...props} addSubscriberHandler={this.addSubscriberHandler} />} />
                  </div>
              </Router>
          )
